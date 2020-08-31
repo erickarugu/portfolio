1b93dd7d8366b730c48769bb95c65671c2168895
erickarugu32 <erickarugu32@gmail.com>
2020-08-31 18:30:27 +0300

    Added projects and connect section

--- app/js/script.js
+++ app/js/script.js
@@ -1,27 +1,29 @@
-const themeToggler = document.querySelector("#theme__toggler");
-const closeAside = document.querySelector("#close__aside");
-const aside = document.querySelector("#navbar__mobile");
-const navbarHamburger = document.querySelector(".navbar__hamburger");
-const body = document.body;
+window.onload = () => {
+  const themeToggler = document.querySelector("#theme__toggler");
+  const closeAside = document.querySelector("#close__aside");
+  const aside = document.querySelector("#navbar__mobile");
+  const navbarHamburger = document.querySelector(".navbar__hamburger");
+  const body = document.body;
 
-themeToggler.addEventListener("click", (e) => {
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-    body.classList.add("light");
+  themeToggler.addEventListener("click", (e) => {
+    if (body.classList.contains("dark")) {
+      body.classList.remove("dark");
+      body.classList.add("light");
 
-    themeToggler.innerHTML = `<i class="fa fa-moon"></i>`;
-  } else {
-    body.classList.remove("light");
-    body.classList.add("dark");
+      themeToggler.innerHTML = `<i class="fa fa-moon"></i>`;
+    } else {
+      body.classList.remove("light");
+      body.classList.add("dark");
 
-    themeToggler.innerHTML = `<i class="fa fa-sun"></i>`;
-  }
-});
+      themeToggler.innerHTML = `<i class="fa fa-sun"></i>`;
+    }
+  });
 
-closeAside.addEventListener("click", (e) => {
-  aside.classList.add("closed");
-});
+  closeAside.addEventListener("click", (e) => {
+    aside.classList.add("closed");
+  });
 
-navbarHamburger.addEventListener("click", (e) => {
-  aside.classList.remove("closed");
-});
+  navbarHamburger.addEventListener("click", (e) => {
+    aside.classList.remove("closed");
+  });
+};
