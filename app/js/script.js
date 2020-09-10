b558bf774785a825b3c7597d7d7a1e666b21b62d
erickarugu32 <erickarugu32@gmail.com>
2020-09-11 00:15:42 +0300

    added work+projects section

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,18 @@
+function openWork(evt, place) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("work-experience");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("work-nav-link");
+  for (i = 0; i < x.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(place).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+
 window.onload = () => {
   const themeToggler = document.querySelector("#theme__toggler");
   const closeAside = document.querySelector("#close__aside");
@@ -27,8 +42,8 @@ window.onload = () => {
     aside.classList.remove("closed");
   });
 
-  if ("serviceWorker" in navigator) {
-    // register service worker
-    navigator.serviceWorker.register("/service-worker.js");
-  }
+  // if ("serviceWorker" in navigator) {
+  //   // register service worker
+  //   navigator.serviceWorker.register("/service-worker.js");
+  // }
 };
