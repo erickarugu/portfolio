303e3695cfa70097200087bb879d437c22687381
erickarugu32 <erickarugu32@gmail.com>
2020-09-13 01:14:49 +0300

    added pojects card js functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,4 @@
-function openWork(evt, place) {
+const openWork = (evt, place) => {
   var i, x, tablinks;
   x = document.getElementsByClassName("work-experience");
   for (i = 0; i < x.length; i++) {
@@ -12,13 +12,44 @@ function openWork(evt, place) {
   evt.currentTarget.className += " active";
 }
 
+const fetchProjects = async(cardsList)=>{
+await fetch('projects.json')
+.then(data => data.json())
+.then(data => renderProjectsCards(data, cardsList))
+.catch(err => console.log(err))
+}
+const renderProjectsCards = (projectsArray, cardsList) => {
+  let str = ""
+  for(key in projectsArray){
+    let card = `
+    <div class="card flex flex-col jc-sb">
+      <div class="card-head flex jc-end">
+        <a href="${projectsArray[key].github_url}" title="GitHub repo"><i class="fab fa-github"></i></a>
+        <a href="${projectsArray[key].live}" title="Visit Link"><i class="fa fa-external-link-alt"></i></a>
+      </div>
+      <div class="card-body">
+        <h3>${key}</h3>
+        <p>${projectsArray[key].description}</p>
+      </div>
+      <div class="card-footer technologies flex jc-sa">${projectsArray[key].tags.map(tag => '<span>'+tag+'</span>').join('')}</div>
+    </div>
+    `
+    str += card;
+  }
+  cardsList.innerHTML = str;
+}
+
 
 window.onload = () => {
   const themeToggler = document.querySelector("#theme__toggler");
   const closeAside = document.querySelector("#close__aside");
   const aside = document.querySelector("#navbar__mobile");
   const navbarHamburger = document.querySelector(".navbar__hamburger");
+
+  const cardsList = document.querySelector("#cards-list");
   const body = document.body;
+
+  fetchProjects(cardsList);
 
   themeToggler.addEventListener("click", (e) => {
     if (body.classList.contains("dark")) {
